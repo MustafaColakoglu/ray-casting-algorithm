2183bfebea170cd77d92c8f7c9eee249d342d50e
MustafaColakoglu <mustafacolakoglu7@gmail.com>
2020-08-13 15:05:31 +0300

    issue fixed

--- ray-casting-algorithm/RayCastingHelper.cs
+++ ray-casting-algorithm/RayCastingHelper.cs
@@ -25,25 +25,34 @@ namespace ray_casting_algorithm
 
         public static bool GetWest(Coordinate A, Coordinate B, double y, double x)
         {
-            if (A.Y <= B.Y)
+            //if (A.Y <= B.Y)
+            //{
+            //    if (y <= A.Y || y > B.Y ||
+            //        x >= A.X && x >= B.X)
+            //    {
+            //        return false;
+            //    }
+            //    else if (x < A.X && x < B.X)
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return (y - A.Y) / (x - A.X) > (B.Y - A.Y) / (B.X - A.X);
+            //    }
+            //}
+            //else
+            //{
+            //    return GetWest(B, A, x, y);
+            //}
+
+            if (((A.Y > y) != (B.Y > y)) && (x < (B.X - A.X) * (y - A.Y) / (B.Y - A.Y) + A.X))
             {
-                if (y <= A.Y || y > B.Y ||
-                    x >= A.X && x >= B.X)
-                {
-                    return false;
-                }
-                else if (x < A.X && x < B.X)
-                {
-                    return true;
-                }
-                else
-                {
-                    return (y - A.Y) / (x - A.X) > (B.Y - A.Y) / (B.X - A.X);
-                }
+                return true;
             }
             else
             {
-                return GetWest(B, A, x, y);
+                return false;
             }
         }
     }
